d0c6ac69951564d0364ad46fdf3771c2653af4c9
Jonathan Bout <jonathanbout01@gmail.com>
2024-03-09 17:36:54 +0100

    fix problems

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/IndexView.vue'
+import IndexView from '@/views/IndexView.vue'
 
 const formatDate = (date: Date) => {
   return `/${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`
 }
 
-const randomDate = () => {
+const randomDateUrl = () => {
   const start = new Date(1995, 5, 16)
   const end = new Date()
   const date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
   return formatDate(date)
 }
 
-const today = () => formatDate(new Date())
+const todayUrl = () => formatDate(new Date())
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,17 +20,17 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      redirect: today
+      redirect: todayUrl
     },
     {
       path: '/:year([0-9]{4})/:month([0-9]{1,2})/:day([0-9]{1,2})',
       name: 'apod',
-      component: HomeView
+      component: IndexView
     },
     {
       path: '/random',
       name: 'picker',
-      redirect: randomDate
+      redirect: randomDateUrl
     },
     {
       path: '/:catchAll(.*)',
