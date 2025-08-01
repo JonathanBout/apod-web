5014c7dd5392e2ef6f44d9981c69432671dd769c
Jonathan Bout <jonathanbout01@gmail.com>
2025-08-01 19:32:17 +0200

    somewhat fix peer deps, update footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="copyright-notice">
     &copy; <a href="https://jonathanbout.com">Jonathan Bout</a>, {{ new Date().getFullYear() }}. All
-    rights for media (images, video's) belong to their respective owners.
+    rights for media (images, videos) belong to their respective owners.
   </footer>
 </template>
